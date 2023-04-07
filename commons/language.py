a35229fa2f5f1989ee3331b65503a61ed0fbb7ba
Corrado Mio <corrado.mio@gmail.com>
2023-04-07 08:38:33 +0400

    Update implementation

--- commons/language.py
+++ commons/language.py
@@ -8,3 +8,23 @@ nil = None
 none = None
 begin = None
 end = None
+
+
+
+def import_from(qname: str) -> type:
+    """
+    Import a class specified by the fully qualified name string
+
+    :param qname: fully qualified name of the class
+    :return: Python class
+    """
+    import importlib
+    p = qname.rfind('.')
+    qmodule = qname[:p]
+    name = qname[p+1:]
+
+    module = importlib.import_module(qmodule)
+    clazz = getattr(module, name)
+    return clazz
+# end
+
