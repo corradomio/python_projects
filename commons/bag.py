2bf9661d1952e9e3883503a632c10c7f7d9da249
Corrado Mio <corrado.mio@gmail.com>
2023-08-26 10:48:11 +0400

    Update implementation

--- commons/bag.py
+++ commons/bag.py
@@ -2,85 +2,100 @@ from builtins import dict
 
 
 # class set(object):
-#     def add(self, *args, **kwargs):
-#     def clear(self, *args, **kwargs):
 #     def copy(self, *args, **kwargs):
-#     def difference(self, *args, **kwargs):
-#     def difference_update(self, *args, **kwargs):
+#
+#     def remove(self, *args, **kwargs):
+#     def clear(self, *args, **kwargs):
+#
+#     def add(self, *args, **kwargs):
 #     def discard(self, *args, **kwargs):
+#
+#     def union(self, *args, **kwargs):
+#     def update(self, *args, **kwargs):
 #     def intersection(self, *args, **kwargs):
 #     def intersection_update(self, *args, **kwargs):
+#     def difference(self, *args, **kwargs):
+#     def difference_update(self, *args, **kwargs):
+#     def symmetric_difference(self, *args, **kwargs):
+#     def symmetric_difference_update(self, *args, **kwargs):
+#
 #     def isdisjoint(self, *args, **kwargs):
 #     def issubset(self, *args, **kwargs):
 #     def issuperset(self, *args, **kwargs):
+#
 #     def pop(self, *args, **kwargs):
-#     def remove(self, *args, **kwargs):
-#     def symmetric_difference(self, *args, **kwargs):
-#     def symmetric_difference_update(self, *args, **kwargs):
-#     def union(self, *args, **kwargs):
-#     def update(self, *args, **kwargs):
 #
 #     def __and__(self, *args, **kwargs):
-#     def __class_getitem__(self, *args, **kwargs):
+#     def __rand__(self, *args, **kwargs):
+#     def __iand__(self, *args, **kwargs):
+#     def __or__(self, *args, **kwargs):
+#     def __ror__(self, *args, **kwargs):
+#     def __ior__(self, *args, **kwargs):
+#     def __sub__(self, *args, **kwargs):
+#     def __rsub__(self, *args, **kwargs):
+#     def __isub__(self, *args, **kwargs):
+#     def __xor__(self, *args, **kwargs):
+#     def __rxor__(self, *args, **kwargs):
+#     def __ixor__(self, *args, **kwargs):
+#
 #     def __contains__(self, y):
 #     def __eq__(self, *args, **kwargs):
-#     def __getattribute__(self, *args, **kwargs):
 #     def __ge__(self, *args, **kwargs):
 #     def __gt__(self, *args, **kwargs):
-#     def __iand__(self, *args, **kwargs):
-#     def __init__(self, seq=()):
-#     def __ior__(self, *args, **kwargs):
-#     def __isub__(self, *args, **kwargs):
-#     def __iter__(self, *args, **kwargs):
-#     def __ixor__(self, *args, **kwargs):
-#     def __len__(self, *args, **kwargs):
 #     def __le__(self, *args, **kwargs):
 #     def __lt__(self, *args, **kwargs):
 #     def __ne__(self, *args, **kwargs):
-#     def __or__(self, *args, **kwargs):
-#     def __rand__(self, *args, **kwargs):
+#
+#     def __init__(self, seq=()):
+#     def __iter__(self, *args, **kwargs):
+#     def __len__(self, *args, **kwargs):
+#
+#     def __getattribute__(self, *args, **kwargs):
+#     def __class_getitem__(self, *args, **kwargs):
 #     def __reduce__(self, *args, **kwargs):
-#     def __repr__(self, *args, **kwargs):
-#     def __ror__(self, *args, **kwargs):
-#     def __rsub__(self, *args, **kwargs):
-#     def __rxor__(self, *args, **kwargs):
 #     def __sizeof__(self):
-#     def __sub__(self, *args, **kwargs):
-#     def __xor__(self, *args, **kwargs):
- 
+#     def __repr__(self, *args, **kwargs):
+
 
 # class dict(object):
 #     def clear(self):
 #     def copy(self):
-#     def get(self, *args, **kwargs):
+#
 #     def items(self):
 #     def keys(self):
+#     def values(self):
+#
+#     def get(self, *args, **kwargs):
 #     def pop(self, k, d=None):
 #     def popitem(self, *args, **kwargs):
 #     def setdefault(self, *args, **kwargs):
-#     def update(self, E=None, **F):
-#     def values(self):
 #
-#     def __class_getitem__(self, *args, **kwargs):
+#     def update(self, E=None, **F):
+#
 #     def __contains__(self, *args, **kwargs):
-#     def __delitem__(self, *args, **kwargs):
 #     def __eq__(self, *args, **kwargs):
-#     def __getattribute__(self, *args, **kwargs):
-#     def __getitem__(self, y):
 #     def __ge__(self, *args, **kwargs):
 #     def __gt__(self, *args, **kwargs):
-#     def __init__(self, seq=None, **kwargs):
-#     def __ior__(self, *args, **kwargs):
-#     def __iter__(self, *args, **kwargs):
-#     def __len__(self, *args, **kwargs):
 #     def __le__(self, *args, **kwargs):
 #     def __lt__(self, *args, **kwargs):
 #     def __ne__(self, *args, **kwargs):
+#
 #     def __or__(self, *args, **kwargs):
+#     def __ror__(self, *args, **kwargs):
+#     def __ior__(self, *args, **kwargs):
+#
+#     def __delitem__(self, *args, **kwargs):
+#     def __getitem__(self, y):
+#     def __setitem__(self, *args, **kwargs):
+#
+#     def __init__(self, seq=None, **kwargs):
+#     def __iter__(self, *args, **kwargs):
+#     def __len__(self, *args, **kwargs):
 #     def __repr__(self, *args, **kwargs):
 #     def __reversed__(self, *args, **kwargs):
-#     def __ror__(self, *args, **kwargs):
-#     def __setitem__(self, *args, **kwargs):
+#
+#     def __getattribute__(self, *args, **kwargs):
+#     def __class_getitem__(self, *args, **kwargs):
 #     def __sizeof__(self):
 
 
