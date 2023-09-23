b5657de90336cd36b6db9a2d55aed03446d29558
Corrado Mio <corrado.mio@gmail.com>
2023-09-23 12:53:24 +0400

    Update implementation

--- commons/sktimex/model/base.py
+++ commons/sktimex/model/base.py
@@ -6,6 +6,10 @@ class ExtendedBaseForecaster(BaseForecaster):
     def __init__(self):
         super().__init__()
 
+    # -----------------------------------------------------------------------
+    #
+    # -----------------------------------------------------------------------
+
     def predict_history(self, fh, X=None, yh=None, Xh=None):
         Xs = None
         ys = None
@@ -18,4 +22,71 @@ class ExtendedBaseForecaster(BaseForecaster):
             self.update(y=ys, X=Xs)
         return prediction
     # end
+
+    # -----------------------------------------------------------------------
+    #
+    # -----------------------------------------------------------------------
+
+    # def fit(self, y, X=None, fh=None):
+    #     pass
+
+    # def predict(self, fh, X=None):
+    #     pass
+
+    # def fit_predict(self, y, X=None, fh=None):
+    #     ...
+
+    # def score(self, y, X=None, fh=None):
+    #     ...
+
+    # def update(self, y, X):
+    #     ...
+
+    # def update_predict(
+    #         self,
+    #         y,
+    #         cv=None,
+    #         X=None,
+    #         update_params=True,
+    #         reset_forecaster=True,
+    # ):
+    #     ...
+
+    # def update_predict_single(
+    #     self,
+    #     y=None,
+    #     fh=None,
+    #     X=None,
+    #     update_params=True,
+    # ):
+    #     ...
+
+    # -----------------------------------------------------------------------
+
+    # def predict_quantiles(self, fh=None, X=None, alpha=None):
+    #     ...
+
+    # def predict_interval(self, fh=None, X=None, coverage=0.90):
+    #     ...
+
+    # def predict_var(self, fh=None, X=None, cov=False):
+    #     ...
+
+    # def predict_proba(self, fh=None, X=None, marginal=True):
+    #     ...
+
+    # def predict_residuals(self, y=None, X=None):
+    #     ...
+
+    # -----------------------------------------------------------------------
+
+    # def update(self, y, X=None, update_params=True):
+    #     ...
+
+    # def update_predict(self, y, cv=None, X=None, update_params=True, reset_forecaster=True):
+    #     ...
+
+    # def update_predict_single(self, y=None, fh=None, X=None, update_params=True):
+    #     ...
+
 # end
