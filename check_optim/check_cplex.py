7bff21855c04b55adc2ebaaf5b0733503dc52d20
Corrado Mio <corrado.mio@gmail.com>
2023-11-26 10:33:34 +0400

    Update implementation

---
+++ check_optim/check_cplex.py
@@ -0,0 +1,39 @@
+import cplex
+import docplex.mp
+
+
+# first import the Model class from docplex.mp
+from docplex.mp.model import Model
+
+def main():
+    # create one model instance, with a name
+    m = Model(name='telephone_production')
+
+    # by default, all variables in Docplex have a lower bound of 0 and infinite upper bound
+    desk = m.continuous_var(name='desk')
+    cell = m.continuous_var(name='cell')
+
+    # write constraints
+    # constraint #1: desk production is greater than 100
+    m.add_constraint(desk >= 100)
+
+    # constraint #2: cell production is greater than 100
+    m.add_constraint(cell >= 100)
+
+    # constraint #3: assembly time limit
+    ct_assembly = m.add_constraint(0.2 * desk + 0.4 * cell <= 400)
+
+    # constraint #4: paiting time limit
+    ct_painting = m.add_constraint(0.5 * desk + 0.4 * cell <= 490)
+
+    m.maximize(12 * desk + 20 * cell)
+
+    m.print_information()
+
+    s = m.solve()
+    m.print_solution()
+
+    pass
+
+if __name__ == "__main__":
+    main()
--- check_optim/check_ortools.py
+++ check_optim/check_ortools.py
@@ -1,5 +1,38 @@
+from ortools.linear_solver import pywraplp
+
+
 def main():
-    pass
+    # Create the linear solver with the GLOP backend.
+    solver = pywraplp.Solver.CreateSolver("GLOP")
+    if not solver:
+        return
+
+    # Create the variables x and y.
+    x = solver.NumVar(0, 1, "x")
+    y = solver.NumVar(0, 2, "y")
+
+    print("Number of variables =", solver.NumVariables())
+
+    # Create a linear constraint, 0 <= x + y <= 2.
+    ct = solver.Constraint(0, 2, "ct")
+    ct.SetCoefficient(x, 1)
+    ct.SetCoefficient(y, 1)
+
+    print("Number of constraints =", solver.NumConstraints())
+
+    # Create the objective function, 3 * x + y.
+    objective = solver.Objective()
+    objective.SetCoefficient(x, 3)
+    objective.SetCoefficient(y, 1)
+    objective.SetMaximization()
+
+    print(f"Solving with {solver.SolverVersion()}")
+    solver.Solve()
+
+    print("Solution:")
+    print("Objective value =", objective.Value())
+    print("x =", x.solution_value())
+    print("y =", y.solution_value())
 
 
 if __name__ == "__main__":
