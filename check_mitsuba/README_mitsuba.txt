32e2fbfdb840c0b9c3035015931940f49d1422fd
Corrado Mio <corrado.mio@gmail.com>
2025-04-08 06:49:00 +0400

    Update implementation

---
+++ check_mitsuba/README_mitsuba.txt
@@ -0,0 +1,14 @@
+
+
+Mitsuba documentation
+    https://mitsuba.readthedocs.io/en/stable/
+
+Scene Format
+    https://mitsuba.readthedocs.io/en/stable/src/key_topics/scene_format.html
+
+Plugins
+    https://mitsuba.readthedocs.io/en/stable/src/plugin_reference.html
+
+    bdrf
+        https://mitsuba.readthedocs.io/en/stable/src/generated/plugins_bsdfs.html
+
--- check_mitsuba/test_scene.py
+++ check_mitsuba/test_scene.py
@@ -7,12 +7,13 @@ mi.set_variant('cuda_ad_rgb')
 
 # scene = mi.load_file("examples/shape_instance_fractal.xml")
 # scene = mi.load_file("examples/dragon/scene_v3.xml")
-scene = mi.load_file("examples/sphere_2.xml")
+scene = mi.load_file("test_scene.xml")
 
 image = mi.render(scene)
 image: np.ndarray = np.array(image)
 image = (image ** (1./2.2)).clip(0,1)
 
+plt.gca().set_aspect('equal')
 plt.axis("off")
 plt.tight_layout()
 plt.imshow(image)
