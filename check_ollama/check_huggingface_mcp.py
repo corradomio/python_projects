727917f5e83ad0b378678af5b05df364c9cc4cc6
Corrado Mio <corrado.mio@gmail.com>
2025-10-24 09:11:05 +0400

    check ollama

--- check_ollama/check_huggingface_mcp.py
+++ check_ollama/check_huggingface_mcp.py
@@ -1,9 +1,12 @@
 from huggingface_hub import MCPClient
 
+api_keys = load(r"D:\Projects.github\python_projects\api_key_tokens.json")
+hf_key = api_keys["hf"]
+
 # Initialize the MCPClient, specifying the server URL or agent ID
 # If connecting to an agent on the Hub, you can use its ID.
 # For a local Gradio MCP server, you'd use its URL (e.g., "http://localhost:7860/mcp")
-client = MCPClient(base_url="https://hf.co/mcp", api_key="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") # or server_url="your-server-url"
+client = MCPClient(base_url="https://hf.co/mcp", api_key=hf_key) # or server_url="your-server-url"
 
 # You can then interact with the client to perform actions,
 # such as getting available tools or running an agent.
